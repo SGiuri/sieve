474ec5b02e8a9c9ec16b07b64c57b29adadc88d7
SGiuri <s.giuri@issm.it>
2020-11-10 22:32:54 +0100

    Initial commit

---
+++ sieve.py
@@ -0,0 +1,12 @@
+def primes(limit):
+    all_numbers = [j for j in range(2, limit + 1)]
+    primes = []
+
+    for j in range(2, limit):
+        if j in all_numbers:
+            primes.append(j)
+        for k in range(j + j, limit + 1, j):
+            if k in all_numbers:
+                all_numbers.remove(k)
+
+    return all_numbers
