894df19db4ff77e849d70135f17d17f31efde843
SGiuri <s.giuri@issm.it>
2020-11-10 22:38:19 +0100

    Corrected and optimized

--- sieve.py
+++ sieve.py
@@ -1,9 +1,8 @@
 def primes(limit):
     all_numbers = [j for j in range(2, limit + 1)]
 
-    for j in range(2, limit):
-        if j in all_numbers:
-            primes.append(j)
+    for j in all_numbers:
+
         for k in range(j + j, limit + 1, j):
             if k in all_numbers:
                 all_numbers.remove(k)
